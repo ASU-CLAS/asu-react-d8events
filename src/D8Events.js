d5895acc313a0a6a8af4534a150ab72ddef5d28f
tkilbour <tyk@asu.edu>
2019-04-12 10:23:39 -0700

    refactored code and started adding additional component style options

--- src/D8Events.js
+++ src/D8Events.js
@@ -2,11 +2,91 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import './D8Events.css';
+import {validDate, formatTime} from './D8Utils'
+
+class EventItemDefault extends Component {
+
+  render() {
+    return (
+
+      <div className="row d8EventRow">
+
+        <div className="d8EventDateContainer">
+
+          <span className="d8EventDateNumber">{validDate(this.props.listNode.very_start_date, 'D')}</span>
+
+          <div className="d8DayOfWeekMonthContainer">
+            <span className="d8EventDayOfWeek">{validDate(this.props.listNode.very_start_date, 'dddd')}</span>
+            <span className="d8EventMonth">{validDate(this.props.listNode.very_start_date, 'MMMM')}</span>
+          </div>
+
+        </div>
+
+        <div className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+
+            {this.props.listNode.image_url !== "" && <a href={`${this.props.listNode.alias}/?eventDate=${validDate(this.props.listNode.very_start_date, 'YYYY-MM-DD')}`} target="_blank">
+              <img src={this.props.listNode.image_url} alt={this.props.listNode.title} className="img-fluid d8EventImage" />
+            </a>}
+
+        </div>
+
+        <div className="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 d8EventDetailsContainer">
+
+            <p className="d8EventTitle">
+              <a href={`${this.props.listNode.alias}/?eventDate=${validDate(this.props.listNode.very_start_date, 'YYYY-MM-DD')}`} target="_blank">{this.props.listNode.title}</a>
+            </p>
+            <p className="d8EventDetails">{formatTime(validDate(this.props.listNode.very_start_date,'h:mm'), validDate(this.props.listNode.very_end_date,'h:mm'))} | {this.props.listNode.campus} | {this.props.listNode.interests.replace(/\|/g, ", ")}</p>
+
+        </div>
+
+      </div>
+
+    );
+  }
+}
+
+class EventItemCard extends Component {
+
+  render() {
+    return (
+
+      <div className="col-12 col-sm-12 col-md-4 eventItemCard">
+
+      <div class="card">
+        <div className="d8EventImageTop-wrapper">
+          <div className="d8EventImageTop">
+            {this.props.listNode.image_url !== "" && <a href={`${this.props.listNode.alias}/?eventDate=${validDate(this.props.listNode.very_start_date, 'YYYY-MM-DD')}`} target="_blank">
+              <img src={this.props.listNode.image_url} alt={this.props.listNode.title} className="img-fluid d8EventImage" />
+            </a>}
+          </div>
+        </div>
+
+        <div className="d8EventDateContainer">
+          <span className="d8EventDateNumber">{validDate(this.props.listNode.very_start_date, 'D')}</span>
+          <div className="d8DayOfWeekMonthContainer">
+            <span className="d8EventDayOfWeek">{validDate(this.props.listNode.very_start_date, 'dddd')}</span>
+            <span className="d8EventMonth">{validDate(this.props.listNode.very_start_date, 'MMMM')}</span>
+          </div>
+        </div>
+
+        <div className="d8EventDetailsContainer">
+            <p className="d8EventTitle">
+              <a href={`${this.props.listNode.alias}/?eventDate=${validDate(this.props.listNode.very_start_date, 'YYYY-MM-DD')}`} target="_blank">{this.props.listNode.title}</a>
+            </p>
+            <p className="d8EventDetails">{formatTime(validDate(this.props.listNode.very_start_date,'h:mm'), validDate(this.props.listNode.very_end_date,'h:mm'))} | {this.props.listNode.campus} | {this.props.listNode.interests.replace(/\|/g, ", ")}</p>
+        </div>
+
+        </div>
+      </div>
+
+    );
+  }
+}
 
 class D8Events extends Component {
 
   state = {
-    ourData: []
+    displayData: []
   };
 
   componentDidMount() {
@@ -18,41 +98,16 @@ class D8Events extends Component {
     axios.get(feedURL).then(response => {
           // console.log(response.data.nodes)
           this.setState({
-            ourData: response.data.nodes,
-            ourItems: feedItems,
+            displayData: response.data.nodes,
+            displayStyle: feedItems,
           })
     })
 
   }
 
-  validDate(dateData, dateFormat) {
-    let returnDate = moment.utc(dateData).format(dateFormat);
-    if(returnDate === "Invalid date") {
-      dateData = dateData.split(':');
-      returnDate = moment.utc(dateData[0]).format(dateFormat);
-      console.log(dateData[1]);
-      console.log(dateFormat);
-      if(dateData[1] === "00Z" && dateFormat === 'h:mm') {
-        returnDate = "All day";
-      }
-    }
-    if(returnDate === "Invalid date") {
-      returnDate = "";
-    }
-    return returnDate;
-  }
-  formatTime(startTime, endTime) {
-    if(startTime === endTime) {
-      return startTime;
-    }
-    else {
-      return startTime + " - " + endTime;
-    }
-  }
-
   render() {
-    // console.log(this.state.ourData);
-    var results = this.state.ourData.map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, start_date: thisNode.node.start_date, campus: thisNode.node.campus, interests: thisNode.node.interests, very_start_date: thisNode.node.very_start_date, very_end_date: thisNode.node.very_end_date, alias: thisNode.node.alias }));
+    // console.log(this.state.displayData);
+    var results = this.state.displayData.map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, start_date: thisNode.node.start_date, campus: thisNode.node.campus, interests: thisNode.node.interests, very_start_date: thisNode.node.very_start_date, very_end_date: thisNode.node.very_end_date, alias: thisNode.node.alias }));
     console.log(results[0]);
     console.log(results[1]);
     console.log(results[2]);
@@ -61,98 +116,53 @@ class D8Events extends Component {
     // have 2018-07-07
     // very_start_date: "2018-07-07T19:30:00Z",
 
-    if (this.state.ourItems==="Three") {
-      return (
-        <div id="D8Events">
+    switch(this.state.displayStyle) {
+
+      case "Three":
+        return (
           <div className="container">
             {results.slice(0,3).map(( listNode, index ) => {
               return(
-
                 <React.Fragment key={index}>
-
-                  <div className="row d8EventRow">
-
-                    <div className="d8EventDateContainer">
-                      <span className="d8EventDateNumber">{this.validDate(listNode.very_start_date, 'D')}</span>
-
-                      <div className="d8DayOfWeekMonthContainer">
-                        <span className="d8EventDayOfWeek">{this.validDate(listNode.very_start_date, 'dddd')}</span>
-                        <span className="d8EventMonth">{this.validDate(listNode.very_start_date, 'MMMM')}</span>
-                      </div>
-
-                    </div>
-
-                    <div className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-
-                        {listNode.image_url !== "" && <a href={`${listNode.alias}/?eventDate=${this.validDate(listNode.very_start_date, 'YYYY-MM-DD')}`} target="_blank"><img src={listNode.image_url} alt={listNode.title} className="img-fluid d8EventImage" /></a>}
-
-                    </div>
-
-                    <div className="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 d8EventDetailsContainer">
-
-                        <p className="d8EventTitle"><a href={`${listNode.alias}/?eventDate=${this.validDate(listNode.very_start_date, 'YYYY-MM-DD')}`} target="_blank">{listNode.title}</a></p>
-                        <p className="d8EventDetails">{this.formatTime(this.validDate(listNode.very_start_date,'h:mm'), this.validDate(listNode.very_end_date,'h:mm'))} | {listNode.campus} | {listNode.interests.replace(/\|/g, ", ")}</p>
-
-                    </div>
-
-                  </div>
-
+                  <EventItemDefault listNode = {listNode} />
                 </React.Fragment>
-
               )
             })}
           </div>
-        </div>
-      )
-    } else {
+        )
+      break;
 
-      return (
-        <div id="D8Events">
-
+      case "ThreeCards":
+        return (
           <div className="container">
-
-                    {results.map(( listNode, index ) => {
-                      return(
-
-                        <React.Fragment key={index}>
-
-                          <div className="row d8EventRow">
-
-                            <div className="d8EventDateContainer">
-                              <span className="d8EventDateNumber">{moment(listNode.very_start_date).format('D')}</span>
-
-                              <div className="d8DayOfWeekMonthContainer">
-                                <span className="d8EventDayOfWeek">{moment(listNode.very_start_date).format('dddd')}</span>
-                                <span className="d8EventMonth">{moment(listNode.very_start_date).format('MMMM')}</span>
-                              </div>
-
-                            </div>
-
-                            <div className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-
-                                {listNode.image_url !== "" && <a href={`${listNode.alias}/?eventDate=${moment(listNode.very_start_date).format('YYYY-MM-DD')}`} target="_blank"><img src={listNode.image_url} alt={listNode.title} className="img-fluid d8EventImage" /></a>}
-
-                            </div>
-
-                            <div className="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 d8EventDetailsContainer">
-
-                                <p className="d8EventTitle"><a href={`${listNode.alias}/?eventDate=${moment(listNode.very_start_date).format('YYYY-MM-DD')}`} target="_blank">{listNode.title}</a></p>
-                                <p className="d8EventDetails">{moment(listNode.very_start_date).format('h:mm')} - {moment(listNode.very_end_date).format('h:mm')} | {listNode.campus} | {listNode.interests.replace(/\|/g, ", ")}</p>
-
-                            </div>
-
-                          </div>
-
-                        </React.Fragment>
-
-                      )
-                    })}
-
+          <div className="row">
+            {results.slice(0,3).map(( listNode, index ) => {
+              return(
+                <React.Fragment key={index}>
+                  <EventItemCard listNode = {listNode} />
+                </React.Fragment>
+              )
+            })}
           </div>
-        </div>
-      );
+          </div>
+        )
+      break;
+
+      default:
+        return (
+          <div className="container">
+            {results.map(( listNode, index ) => {
+              return(
+                <React.Fragment key={index}>
+                  <EventItemDefault listNode = {listNode} />
+                </React.Fragment>
+              )
+            })}
+          </div>
+        );
 
     }
+
 
   }
 }
---
+++ src/D8Utils.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+
+export const validDate = (dateData, dateFormat) => {
+  let returnDate = moment.utc(dateData).format(dateFormat);
+  if(returnDate === "Invalid date") {
+    dateData = dateData.split(':');
+    returnDate = moment.utc(dateData[0]).format(dateFormat);
+    console.log(dateData[1]);
+    console.log(dateFormat);
+    if(dateData[1] === "00Z" && dateFormat === 'h:mm') {
+      returnDate = "All day";
+    }
+  }
+  if(returnDate === "Invalid date") {
+    returnDate = "";
+  }
+  return returnDate;
+}
+export const formatTime = (startTime, endTime) => {
+  if(startTime === endTime) {
+    return startTime;
+  }
+  else {
+    return startTime + " - " + endTime;
+  }
+}
